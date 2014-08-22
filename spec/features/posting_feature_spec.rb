1ab3b8713b51fdb91086463bdda6a49e5f1a07f0
Jeremy Fox <foxjerem@gmail.com>
2014-08-22 20:34:35 +0100

    Added post model

--- spec/features/posting_feature_spec.rb
+++ spec/features/posting_feature_spec.rb
@@ -5,25 +5,36 @@ describe 'Posting:' do
 	context 'when there are no posts' do
 
 		it 'should display a message' do
-			visit '/'
+			visit posts_path
 			expect(page).to have_content('No posts yet')
 		end
 
 		it 'should have a link to add posts' do
-			visit '/'
+			visit posts_path
 			expect(page).to have_css('a', text: 'New Post')
 		end
 
-		xit 'should allow a user to add posts' do
-
+		it 'should allow a user to add a text post' do
+			visit posts_path
+			click_link 'New Post'
+			fill_in 'Title', with: 'text post'
+			fill_in 'Text', with: 'waffle waffle'
+			click_on 'Post'
+			expect(page).to have_content('Thank you for posting...')
 		end
 	
 	end
 
 	context 'when there are posts' do
 
-		xit 'should display them on the homepage' do
+		before(:each) { Post.create(title: 'text post', text: 'waffle waffle')}
 
+		it 'should display them on the homepage' do
+			visit posts_path
+			within('#post-body') do
+				expect(page).to have_content('text post')
+				expect(page).to have_content('waffle waffle')
+			end 
 		end
 
 	end
