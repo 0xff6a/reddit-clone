c772e1e6efdc608c85d6d86405fe207528ea94d0
Jeremy Fox <foxjerem@gmail.com>
2014-08-23 21:27:55 +0100

    Refactored tests

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -7,14 +7,14 @@ describe 'User management:' do
 		it 'a user can sign up' do
 			_sign_up_test_user
 			expect(page).to have_content('Welcome! You have signed up successfully')
-			expect(page).to have_content('test@test.com')
+			expect(page).to have_welcome_message
 		end
 
 		it 'a user can sign in' do
 			create(:test_user)
 			_sign_in_test_user
 			expect(page).to have_content('Signed in successfully')
-			expect(page).to have_content('test@test.com')
+			expect(page).to have_welcome_message
 		end
 
 	end
@@ -26,7 +26,7 @@ describe 'User management:' do
 		it 'a user can sign out' do
 			_sign_out
 			expect(page).to have_content('Signed out successfully')
-			expect(page).not_to have_content('test@test.com')
+			expect(page).not_to have_welcome_message
 		end
 
 	end
@@ -36,27 +36,15 @@ describe 'User management:' do
 		before(:each) { create(:test_post) }
 
 		it 'a user must be signed in to post' do
-			visit posts_path
-			expect(page).not_to have_posting_links
-			_create_and_login_test_user
-			visit posts_path
-			expect(page).to have_posting_links
+			must_be_signed_in_to?(have_posting_links)
 		end
 
 		it 'a user must be signed in to comment' do
-			visit posts_path
-			expect(page).not_to have_commenting_links
-			_create_and_login_test_user
-			visit posts_path
-			expect(page).to have_commenting_links
+			must_be_signed_in_to?(have_commenting_links)
 		end
 
 		it 'a user must be signed in to vote' do
-			visit posts_path
-			expect(page).not_to have_voting_links
-			_create_and_login_test_user
-			visit posts_path
-			expect(page).to have_voting_links
+			must_be_signed_in_to?(have_voting_links)
 		end
 
 	end
--- spec/support/general_support.rb
+++ spec/support/general_support.rb
@@ -3,6 +3,10 @@ def _create_and_login_test_user
 	login_as user
 end
 
+def have_welcome_message
+	have_content('Welcome test@test.com')
+end
+
 def have_posting_links
 	have_css('a', text: 'Post New Text') && have_css('a', text: 'Post New Link')
 end
@@ -13,4 +17,12 @@ end
 
 def have_voting_links
 	have_css('.votes-link')
+end
+
+def must_be_signed_in_to?(have_privilege)
+	visit posts_path
+	expect(page).not_to have_privilege
+	_create_and_login_test_user
+	visit posts_path
+	expect(page).to have_privilege
 end
